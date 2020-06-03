ef8e9fc4d552984c77e47a9f2340a26a356b5620
Miguel <alumno.185604@iesazarquiel.es>
2020-06-03 12:27:49 +0200

    prueba

--- ConsoleAppHelloWorld/ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/ConsoleAppHelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
